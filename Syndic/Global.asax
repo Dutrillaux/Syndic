2f23b4bd4799ed852b37a1742873dfc378893bfd
l.dutrillaux <l.dutrillaux@FR-VM-XAM01>
2016-12-02 17:31:16 +0100

    Add project files.

---
+++ Syndic/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Syndic.MvcApplication" Language="C#" %>
---
+++ Syndic/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Syndic.Startup))]
+namespace Syndic
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
